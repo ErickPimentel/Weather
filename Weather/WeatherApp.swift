2c8159a0ca6282b315e744d65efdb82827a9bd58
Erick Spinelli Pimentel <erickspinellipimentel@gmail.com>
2022-11-06 20:44:34 -0600

    first commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Erick Spinelli Pimentel on 11/6/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
