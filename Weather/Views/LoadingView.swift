5f22fd9bd2410db21b987426835dea9e262991ac
Erick Spinelli Pimentel <erickspinellipimentel@gmail.com>
2022-11-06 21:26:44 -0600

    feat: LoadingView

---
+++ Weather/Views/LoadingView.swift
@@ -0,0 +1,22 @@
+//
+//  LoadingView.swift
+//  Weather
+//
+//  Created by Erick Spinelli Pimentel on 11/6/22.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
