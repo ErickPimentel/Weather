d67722420632b20236a1dbed53284eae79b965a9
Erick Spinelli Pimentel <erickspinellipimentel@gmail.com>
2022-11-08 15:16:48 -0600

    feat: WeatherView

---
+++ Weather/Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  Weather
+//
+//  Created by Erick Spinelli Pimentel on 11/8/22.
+//
+
+import Foundation
+
+extension Double {
+    func rounDouble() -> String {
+        return String(format: "%.0f", self)
+    }
+}
